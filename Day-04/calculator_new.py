4d63fd55465968b0f4dda29c487dd9bc8d20b37a
NickyAsaph <oyathekhuanicholas@gmail.com>
2024-08-19 15:00:14 -0500

    day 4

---
+++ Day-04/calculator_new.py
@@ -0,0 +1,17 @@
+def addition(num1, num2):
+    add = num1 + num2 
+    return add
+
+def sub(num1, num2):
+    s = num1 - num2
+    return s   
+
+def mul(num1, num2):
+    m = num1 * num2
+    return m
+
+print(addition(5, 10))
+
+print(sub(10, 5))
+
+print(mul(10, 5))
